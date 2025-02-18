a800dea2ab5638e27c740e92596e1b80c504b1c3
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-18 18:40:41 -0300

    uo

--- Exe06_RafaelMagalhaes.py
+++ Exe06_RafaelMagalhaes.py
@@ -1,10 +1,9 @@
-#Peça um número a baixo de 50 e em seguida faça uma contagem regressiva até sse número, certificando-se , de demonstrar o número que ele inseriu a saida 
+#Peça um número a baixo de 50 e em seguida faça uma contagem regressiva até esse número, certificando-se , 
+# de demonstrar o número que ele inseriu a saida 
 
 num = int (input("Digite um número a baixo de 50: "))
 
-for i in num:
-     num = num - 1
-     print(num)
+for i in range(50,num - 1, -1):
+     print(i)
 
-
-plçççççççççççççççç
+     AAAAAAAAAAAA
--- Exe08_RafaelMagalhaes.py
+++ Exe08_RafaelMagalhaes.py
@@ -1,3 +1,12 @@
 #defina uma variavel chamada total como 0. 
 # Peça ao ususario para inserir 5 números e, apos cada entrada pergunte se ele deseja se esse numero seja incluido('S' ou 's', 'N' ou 'n')
-#Se ele desejar adicone o n~úmero ao total. Se ele nao quiser inclui-lo nao o adicone ao total. Depos de inserir 5 n´´úmeros exiba o total
+#Se ele desejar adicone o número ao total. Se ele nao quiser inclui-lo nao o adicone ao total. Depos de inserir 5 n´´úmeros exiba o total
+
+total = 0
+
+num1 = (input("Digite o primeiro número: "))
+num2 = (input("Digite o segundo número: "))
+num3 = (input("Digite o terceiro número: "))
+num4 = (input("Digite o quarto número: "))
+num5 = (input("Digite o quinto número: "))
+
